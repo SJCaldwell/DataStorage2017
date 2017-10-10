4d92ac101e20365c8b8069311326226b1dc79150
SJCaldwell <shane.caldwell12@ncf.edu>
2017-10-10 14:39:25 -0400

    Creating initial setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+  name="RivalsForLife",
+  version="0.1dev",
+  packages=['rivalsforlife',],
+  license="MIT", 
+  long_description=open('README.MD').read(),
+)
