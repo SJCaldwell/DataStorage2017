17e294d7d3c9987a39f7f242415986e69ed562e0
SJCaldwell <shane.caldwell12@ncf.edu>
2017-10-22 21:49:37 -0400

    Configured app to work with Flask Login Manager
    - Batteries yet to be included

--- server/saltysplatoon.py
+++ server/saltysplatoon.py
@@ -19,6 +19,11 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route("/")
 def greetings():
