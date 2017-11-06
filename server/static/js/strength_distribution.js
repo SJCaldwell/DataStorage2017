53ac63d9ef5a7c641bf03d7be40d22e409971b31
SJCaldwell <shane.caldwell12@ncf.edu>
2017-11-06 16:23:06 -0500

    jQuery is now grabbing data from the server

--- server/static/js/strength_distribution.js
+++ server/static/js/strength_distribution.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-
-	console.log("Document here")
-	$("#strength_stats").submit(function(){
+	$("#strength_button").click(function(){
 		console.log("SEE SUBMIT");
 		$.ajax({
 			type: "POST",
 			url: "strength_distribution",
-			data: $(this).serialize(),
+			data: $("#strength_stats").serialize(),
 			success: function(data){
 				var distribution = data;
 				console.log(distribution);
