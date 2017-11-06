cbab1e7292979ee410268008dfc7369ad7f61b36
SJCaldwell <shane.caldwell12@ncf.edu>
2017-11-06 12:07:25 -0500

    Deleted unneeded input

--- server/saltysplatoon.py
+++ server/saltysplatoon.py
@@ -22,9 +22,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from saltysplatoon import db, bcrypt
-
-
 class Meets(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	federation = db.Column(db.Text)
